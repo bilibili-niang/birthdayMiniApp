75b6174aae9dd639f76bb0bdb04b5679b0228d2a
icestone <killicestone@126.com>
2025-08-20 21:36:04 +0800

    refactor(demo.sh): 优化 GitHub 主机密钥添加方式
    - 使用 ssh-keyscan 自动获取并添加 GitHub 主机密钥
    - 更新相关注释以反映新的实现方式

--- demo.sh
+++ demo.sh
@@ -18,9 +18,9 @@ echo "[DEBUG] 设置Git配置"
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
-# 添加 GitHub 的主机密钥
-echo "[DEBUG] 添加 GitHub 的主机密钥"
-echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> ~/.ssh/known_hosts
+# 自动获取并添加 GitHub 的主机密钥
+echo "[DEBUG] 自动扫描并添加 GitHub 的主机密钥"
+ssh-keyscan github.com >> ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
 
 gitPath="/www/wwwroot/$projectName"
