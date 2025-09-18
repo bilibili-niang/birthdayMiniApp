92082544beebea84ce190a124f70d6b8986f4971
icestone <killicestone@126.com>
2025-09-19 00:22:16 +0800

    refactor: 删除未使用的代码文件
    - 删除了多个未使用的代码文件，包括 404 页面、微信小程序认证、基本布局等
    - 清理了不必要的样式文件和 TypeScript 定义
    - 移除了无用的组件和 API 接口

--- admin/template/core/src/api/fund-account/index.ts
+++
@@ -1,9 +0,0 @@
-import request from '../request'
-
-/** 获取资金账户信息 */
-export const $getFundAccountInfo = () => {
-  return request({
-    url: '/kacat-cornerstone-merchant/wallet',
-    method: 'get'
-  })
-}
