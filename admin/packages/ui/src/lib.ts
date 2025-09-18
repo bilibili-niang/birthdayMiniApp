0b8cae1e34376aee11cad64c82505e5065f0539d
icestone <killicestone@126.com>
2025-09-18 21:39:30 +0800

    build(admin-ui): 重构项目并移除 Vite 配置- 新增 env.d.ts 文件以引用 Vite 客户端类型 - 重构 lib.ts 文件，移除冗余导入和导出 - 更新 package.json，添加 module 和 exports 字段 - 更新 tsconfig.json，增加 Vuetify 类型引用 - 删除 vite.config.mts 文件，移除 Vite配置

--- admin/packages/ui/src/lib.ts
+++ admin/packages/ui/src/lib.ts
@@ -1,8 +1,8 @@
-import * as vuetify from 'vuetify'
 import './styles/settings.scss'
 import 'vuetify/_styles.scss'
 
 export { default as Button } from './components/button/index'
+
 export { default as Input } from './components/input/index'
 export { default as List } from './components/list/index'
 export { default as ListItem } from './components/listItem/index'
@@ -10,7 +10,5 @@ export { default as ListItemSubtitle } from './components/listItemSubtitle/index
 export { default as ListItemTitle } from './components/listItemTitle/index'
 export { default as Modal } from './components/modal/index'
 export { createModal } from './components/modal/service'
-export { notify, notifySuccess, notifyError, installNotify } from './components/notify/index'
-
-// @ts-ignore
-export { vuetify }
+export { notify, notifySuccess, installNotify, notifyError } from './components/notify/index'
+export * from 'vuetify'
